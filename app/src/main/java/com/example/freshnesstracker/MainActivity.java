a92f10773fbb278cdcc5989dd6eb9cd53e098867
chilla2 <64.aprilhill@gmail.com>
2021-02-16 12:13:07 -0700

    Test change - Added comment line 9 of MainActivity

--- app/src/main/java/com/example/freshnesstracker/MainActivity.java
+++ app/src/main/java/com/example/freshnesstracker/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //April's test comment
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
