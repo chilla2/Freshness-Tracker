7714fc6e4413067b9978844ba1ad9a923aff88a3
chilla2 <64.aprilhill@gmail.com>
2021-02-16 12:32:00 -0700

    Still testing. Changing line 9 completely.

--- app/src/main/java/com/example/freshnesstracker/MainActivity.java
+++ app/src/main/java/com/example/freshnesstracker/MainActivity.java
@@ -6,8 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //April's test comment ---Hadassah Comments --Jacque's comment - Michael's comment
-
+    //My notes are the best notes.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
