688370fd9d70c64425a888141f1de8b6dab42c40
hgriffin3 <78107762+hgriffin3@users.noreply.github.com>
2021-02-16 12:19:22 -0700

    Hadassah Comments in a Line

--- app/src/main/java/com/example/freshnesstracker/MainActivity.java
+++ app/src/main/java/com/example/freshnesstracker/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //April's test comment
+    //April's test comment ---Hadassah Comments
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
