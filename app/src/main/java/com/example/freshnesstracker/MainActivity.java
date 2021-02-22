c137ce7f7a4f95dcd4ef769141995cd1209a10db
April <64.aprilhill@gmail.com>
2021-02-22 16:05:52 -0700

    Adding incomplete createFooodItem member function in Main Activity.

--- app/src/main/java/com/example/freshnesstracker/MainActivity.java
+++ app/src/main/java/com/example/freshnesstracker/MainActivity.java
@@ -6,9 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
     ListManager mainInventory;
     ArrayAdapter<String> arrayAdapter;
+    private static final String FILE_NAME = "food-item-list";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +30,46 @@ public class MainActivity extends AppCompatActivity {
     }
     private void onSearch(View view){
 
+    }
+
+    private void createFoodItem(String name, Integer date, String category) {
+
+        JSONObject foodItem = new JSONObject();
+        try {
+            foodItem.put("Name", name);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            foodItem.put("Expiration Date", date);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            foodItem.put("Category", category);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        // Convert JsonObject to String Format
+        String foodItemString = foodItem.toString();
+        // Define the File Path and its Name
+        File file = new File(this.getFilesDir(),FILE_NAME);
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        try {
+            bufferedWriter.write(foodItemString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
