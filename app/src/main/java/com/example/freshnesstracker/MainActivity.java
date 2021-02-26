927592b2a37955152d63cc4de12db76a2ba7ca5f
April <64.aprilhill@gmail.com>
2021-02-26 12:35:45 -0700

    Trying to add code to create and edit JSON file

--- app/src/main/java/com/example/freshnesstracker/MainActivity.java
+++ app/src/main/java/com/example/freshnesstracker/MainActivity.java
@@ -21,6 +21,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Date;
+//test change
 
 
 public class MainActivity extends AppCompatActivity {
