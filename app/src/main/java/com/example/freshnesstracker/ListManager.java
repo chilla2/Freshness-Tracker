01f9abce54ce76456e8fe4d0bd00f9e0706c5dff
patnjacq <yan20004@byui.edu>
2021-02-26 09:43:51 -0800

    Update of ListManager sortByExpiry() and searchByType()

--- app/src/main/java/com/example/freshnesstracker/ListManager.java
+++ app/src/main/java/com/example/freshnesstracker/ListManager.java
@@ -1,15 +1,36 @@
 package com.example.freshnesstracker;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class ListManager {
     ArrayList<FoodItem> inventory;
 
     private void sortByExpiry(ArrayList<FoodItem> foodList){
-        // function will sort the ArrayList by date.
+
+        Comparator<FoodItem> dateComparator = new Comparator<FoodItem>() {
+            @Override
+            public int compare(FoodItem o1, FoodItem o2) {
+                return o1.date.compareTo(o2.date);
+            }
+        };
+
+        Collections.sort(inventory, dateComparator);
     }
+
     public ArrayList<FoodItem> searchByType(FoodType  foodType){
         // function will create a new list(new ListManager?) with only specified type and then sort it by date
+       /* sortByExpiry();
+
+        ArrayList<FoodItem>  listByType = new ArrayList<>();
+        for(FoodItem i : inventory){
+            if (i.getFoodType() == foodType){
+                listByType.add(i);
+            }
+        }
+        return listByType;*/
+
         return null;
     }
 
