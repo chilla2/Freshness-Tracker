6384b6eaaac8f2b965c9d97514efb70492b44490
MichaelSilski <sil16005@byui.edu>
2021-03-03 14:16:19 -0700

    Created Search activity and fixed names for add and edit activities.

--- app/src/main/java/com/example/freshnesstracker/SearchActivity.java
+++ app/src/main/java/com/example/freshnesstracker/SearchActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class editItemActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_item);
+        setContentView(R.layout.activity_search);
     }
 }
