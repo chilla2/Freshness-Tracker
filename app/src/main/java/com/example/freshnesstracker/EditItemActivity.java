7d34c09283b7d8c44e830c7f52c908255ce0862d
MichaelSilski <sil16005@byui.edu>
2021-03-05 23:07:30 -0700

    Updated the displays for the AddItemActivity and MainActivity (they both should be perfectly ready to be integrated with the rest of the code). Updated the EditItemActivity and its display, should also be ready for integration.

--- app/src/main/java/com/example/freshnesstracker/EditItemActivity.java
+++ app/src/main/java/com/example/freshnesstracker/EditItemActivity.java
@@ -3,12 +3,26 @@ package com.example.freshnesstracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class EditItemActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_item);
+        setContentView(R.layout.activity_add_item);
+        Spinner spinner = (Spinner) findViewById(R.id.foodType);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.foodTypeList, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+
+    public void onRadioButtonClicked(View view) {
     }
 }
