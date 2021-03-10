1cb1aba1387b02af95fa3c1142c55079e7133df8
April <64.aprilhill@gmail.com>
2021-03-10 11:57:11 -0700

    Took the artist/track tutorial from online and adapted it for our purposes. Everything is still happening in MainActivity, and the xml layout needs work. User can enter food name, select category from dropdown, and choose date from date picker. Temporary add button successfully adds item to database. Items are displayed in a list view. Long press on an item brings up a dialog with options to update or delete item. The delete button works to remove the item from the database/list view, but the update button still needs work. It crashes the app.

--- app/src/main/java/com/example/freshnesstracker/MainActivity.java
+++ app/src/main/java/com/example/freshnesstracker/MainActivity.java
@@ -15,14 +15,10 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -30,7 +26,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import android.widget.DatePicker;
 
